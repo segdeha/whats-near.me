15194208d0efb043844e205f33f0184d6688be05
Andrew Hedges <andrew@hedges.name>
2020-04-05 20:38:35 -0700

    Map shows places in the right spots and centers on user's location

--- src/Map.js
+++ src/Map.js
@@ -1,54 +1,65 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import getNearby from './lib/getNearby';
-import Places from './Places';
-import Me from './Me';
+
+const Me = () => <i>•</i>;
+
+const Pin = ({ title, lat, lng }) => <div className="Map-pin" lat={ lat } lng={ lng }>{ title }</div>;
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      center: { // the kennedy school
+        lat: 45.564455,
+        lng: -122.630576
+      },
+      zoom: 14,
       places: []
     };
   }
 
-  static defaultProps = {
-    center: { // the kennedy school
-      lat: 45.564455,
-      lng: -122.630576
-    },
-    zoom: 15
-  };
-
   componentDidMount() {
     let newNearbyPlaces = loc => {
       const { latitude, longitude } = loc.coords;
+      const { map, maps } = this.state;
+      if (map && maps) {
+        map.panTo(new maps.LatLng(latitude, longitude));
+      }
       let places = getNearby(latitude, longitude);
       places.then(json => {
-
-// console.log('json.query.pages[0]', json.query.pages[0])
-
-          this.setState({
-            places: json.query.pages
-          });
+        this.setState({
+          places: json.query.pages
         });
-      };
-
-      let geo = navigator.geolocation.watchPosition(newNearbyPlaces)
+      });
+    };
+    let geo = navigator.geolocation.watchPosition(newNearbyPlaces)
   }
 
   render() {
-    let { places } = this.state;
-    let { center, zoom } = this.props;
+    let { places, center, zoom } = this.state;
     return (
       <div style={{ height: '100vh', width: '100vw' }}>
         <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyDvSl4yqevaoY63RJwzf74Civuq4uCBWf0' }}
             defaultCenter={ center }
             defaultZoom={ zoom }
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => {
+              this.setState({ map, maps });
+            }}
           >
           <Me center={ center } />
-          <Places places={ places } />
+          {places.map(place => {
+            const { pageid, title, coordinates } = place;
+            if (coordinates.length > 0) {
+              let { lat, lon } = coordinates[0];
+              return <Pin key={ pageid } title={ title } lat={ lat } lng={ lon } />
+            }
+            else {
+              return null;
+            }
+          })}
         </GoogleMapReact>
       </div>
     );
--- src/Me.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-
-const Me = () => <i>•</i>;
-
-export default Me;
