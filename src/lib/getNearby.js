8296f00f8caf70ebadef02a78d86373351e8f91a
Andrew Hedges <andrew@hedges.name>
2020-04-05 19:39:35 -0700

    Clean up comments

--- src/lib/getNearby.js
+++ src/lib/getNearby.js
@@ -2,8 +2,7 @@ const getNearby = async (lat, lng) => {
     let url = `https://segdeha.com/api/nearby.php?lat=${lat}&lng=${lng}`;
     let response = await fetch(url);
 
-    if (response.ok) { // if HTTP-status is 200-299
-        // get the response body (the method explained below)
+    if (response.ok) { // HTTP status is 200-299
         let json = await response.json();
         return json;
     }
