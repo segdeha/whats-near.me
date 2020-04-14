2d1ef3526a62d93079bfe2ace6f8dc49266a43db
Andrew Hedges <andrew@hedges.name>
2020-04-13 21:34:53 -0700

    Add text prompting the user to add the app to their home screen

--- src/Info.js
+++ src/Info.js
@@ -18,14 +18,30 @@ const Info = props => {
   else {
     return (
       <div className="App-info-container">
-        <h1>Welcome!</h1>
-        {
-          geo ? <p><button disabled>Location shared!</button></p>
-              : <p><button onClick={ () => allowGeo(true) }>Share Location</button></p>
-        }
-        <p><strong>Click “Share Location”</strong> to discover interesting landmarks around you from Wikipedia.</p>
-        <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
-        <p><em>Note: Your location is never stored in the app or on our servers!</em></p>
+        <h1>
+          {
+            geo ? <button disabled>Location shared!</button>
+                : <button onClick={ () => allowGeo(true) }>Share Location</button>
+          }
+        </h1>
+        <article>
+          <p><strong>Click “Share Location”</strong> to discover interesting landmarks around you from Wikipedia.</p>
+          <p><strong>Tap on a marker</strong> on the map and more information about it will show up in this space. Have fun!</p>
+          <p><em>Note: Your location is never stored in the app or on our servers!</em></p>
+          <p><strong>What’s near me?</strong> looks and works best when installed on your phone as an app.</p>
+          <details>
+            <summary>How to: iOS</summary>
+            <ol>
+              <li>In Safari, tap the share icon at the bottom middle of the window.</li>
+            </ol>
+          </details>
+          <details>
+            <summary>How to: Android</summary>
+            <ol>
+              <li>In Chrome, tap the three dots at the top right of the window.</li>
+            </ol>
+          </details>
+        </article>
       </div>
     );
   }
